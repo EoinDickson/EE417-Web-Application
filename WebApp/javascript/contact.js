8c6aba7d7ce3626f1edd936af44ac4496ddd36e0
Eoin Dickson <eoindickson1355@gmail.com>
2023-02-19 19:27:19 +0000

    Assignment2

--- WebApp/javascript/contact.js
+++ WebApp/javascript/contact.js
@@ -1,8 +1,7 @@
 
-const form = document.getElementById("register");
-const userName = document.getElementById("username");
+const form = document.getElementById("contact_form");
 
-let loginId = 0;
+let contactId = 0;
 form.addEventListener('submit', e => {
     e.preventDefault();
     console.log("listerner");
@@ -13,9 +12,10 @@ form.addEventListener('submit', e => {
         const obj = Object.fromEntries(fd);
         const json = JSON.stringify(obj);
 
-        localStorage.setItem("login" + loginId, json);
-        console.log(localStorage.getItem("login" + loginId));
-        loginId++
+        localStorage.setItem("contact" + contactId, json);
+        console.log(localStorage.getItem("contact" + contactId));
+        contactId++
+        document.getElementById("contact_form").reset();
     }
 
 });
@@ -25,15 +25,18 @@ function ouputLocal() {
 
     for (let i = 0; i < numLocalStorageElements; i++) {
         console.log(localStorage.getItem(i));
-
     }
 }
 
-function loginTable() {
-    document.getElementById("loginTable").removeChild(document.getElementById("loginTable").lastChild);
+// function clear(){
+//     document.getElementById("contact_form").reset();
+//     console.log("cleared");
+// }
 
-    console.log("loginTable pressed");
-    let obj = JSON.parse(localStorage.getItem("login" + 0));
+function contactTable() {
+    document.getElementById("contactTable").removeChild(document.getElementById("contactTable").lastChild);
+
+    let obj = JSON.parse(localStorage.getItem("contact" + 0));
 
     const table = document.createElement('table');
 
@@ -46,7 +49,7 @@ function loginTable() {
         row.appendChild(th);
     }
     for (let i = 0; i < localStorage.length; i++) {
-        obj = JSON.parse(localStorage.getItem("login" + i))
+        obj = JSON.parse(localStorage.getItem("contact" + i))
 
         const tbody = table.createTBody();
         const dataRow = tbody.insertRow(0);
@@ -58,16 +61,12 @@ function loginTable() {
 
     }
     console.log("asdf pressed");
-    document.getElementById("loginTable").appendChild(table);
+    document.getElementById("contactTable").appendChild(table);
     // document.body.appendChild(table);
 
 
 }
 
-userName.onfocus = () => {
-    console.log("focused");
-    setSuggestion(userName, "John");
-};
 
 const setError = (element, message) => {
     const inputControl = element.parentElement;
@@ -93,30 +92,23 @@ const resetSuggestion = (element) => {
 
 
 function validateInputs() {
-    const userName = document.getElementById("username");
     const firstName = document.getElementById("firstName");
     const secondName = document.getElementById("secondName");
     const email = document.getElementById("email");
     const phone = document.getElementById("phone");
-    const dob = document.getElementById("dob");
-    const gender = document.getElementById("gender");
-    const address1 = document.getElementById("address1");
-    const country = document.getElementById("country");
-    const password = document.getElementById("password");
-    const confirmPassword = document.getElementById("confirmPassword");
+    const ref = document.getElementById("ref");
+    const subject = document.getElementById("subject");
+    const message = document.getElementById("message");
+    const terms = document.getElementById("terms");
+    const mailing = document.getElementById("mailing");
+    const contact = document.getElementById("contact");
+
 
     let valid = true;
 
-    if (userName.value === '') {
-        valid = false;
-        setError(userName, "Please Enter a Valid Username.")
-    } else {
-        setError(userName, '');
-    }
-
     if (firstName.value === '') {
         valid = false;
-        setError(firstName, "Please Enter a Valid Second Name")
+        setError(firstName, "Please Enter a Valid First Name")
     }
     else {
         setError(firstName, '');
@@ -148,77 +140,35 @@ function validateInputs() {
     else {
         setError(phone, '');
     }
-
-    if (dob.value === '') {
-        valid = false;
-
-        setError(dob, "Please Enter a Valid Date of Birth")
-    }
-    else {
-        setError(dob, '');
-    }
-
-    if (gender.value === '') {
-        valid = false;
-
-        setError(gender, "Please Enter a Valid Gender")
-    }
-    else {
-        setError(gender, '');
-    }
-
-    if (address1.value === '') {
-        valid = false;
-
-        setError(address1, "Please Enter a Valid Address")
-    }
-    else {
-        setError(address1, '');
-    }
-
-    if (country.value === '') {
-        valid = false;
-
-        setError(country, "Please Enter a Valid Country")
-    }
-    else {
-        setError(country, '');
-    }
-
-    if (password.value != confirmPassword.value) {
-        valid = false;
-
-        setError(password, "Passords are not the same. Please retry.")
-        setError(confirmPassword, "Passords are not the same. Please retry.")
-    }
-    else {
-        setError(password, '');
-        setError(confirmPassword, '');
-    }
-
-    if (password.value === '') {
-        valid = false;
-
-        setError(password, "Please Enter a Password")
-    }
-    else {
-        setError(password, '');
-    }
-    if (confirmPassword.value === '') {
-        valid = false;
-
-        setError(confirmPassword, "Please Confirm Password")
-    }
-    else {
-        setError(confirmPassword, '');
-    }
     if (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(phone.value)){
         setError(phone,'');
     }else{
         setError(phone,'Invalid Phone number');
-
     }
+    if (subject.value === '') {
+        valid = false;
 
+        setError(subject, "Please don't leave subject empty.")
+    }
+    else {
+        setError(subject, '');
+    }
+    if (message.value === '') {
+        valid = false;
+
+        setError(message, "Please don't leave message empty.")
+    }
+    else {
+        setError(message, '');
+    }
+    if (terms.value === '') {
+        valid = false;
+
+        setError(terms, "Please agree to terms and conditions")
+    }
+    else {
+        setError(terms, '');
+    }
     return valid;
 
 }
--- WebApp/javascript/site.js
+++ WebApp/javascript/site.js
@@ -1,17 +1,22 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
-  function overDropdown(x) {
-    const content = document.getElementById("dropdown-content");
-    content.style.display = "block";
-  }
-  function outDropdown(x) {
-    const content = document.getElementById("dropdown-content");
-    content.style.display = "none";
-  }
+  document.getElementById("nav").innerHTML = sidebar;
+  document.getElementById("nav").style.opacity = "1";
+}
+
+function closeNav() {
+  document.getElementById("nav").style.opacity = '0';
+  document.getElementById("nav").innerHTML = '';
+}
+function overDropdown(x) {
+  const content = document.getElementById("dropdown-content");
+  content.style.display = "block";
+}
+function outDropdown(x) {
+  const content = document.getElementById("dropdown-content");
+  content.style.display = "none";
+}
+
+// document.getElementById("nav").removeChild(document.getElementById("nav").lastChild);
+
+const sidebar = '<div id="mySidenav" class="sidenav"><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a><a href="index.html">Home</a><a href="gallery.html">Gallery</a><a href="leaderboard.html">Leaderboard</a><div class="dropdown"><a class="dropbtn" onmouseover="overDropdown(this)" onmouseout="outDropdown(this)">Info<i class="fa fa-caret-down"></i></a><div class="dropdown-content" id="dropdown-content" onmouseover="overDropdown(this)" onmouseout="outDropdown(this)"><a href="about.html">About Us</a><a href="contact.html">Contact</a></div></div><div class="bottom-nav"><hr><a href="profile.html"><i class="fa fa-user"></i></a><a href="login.html">Login / Logout</a></div></div>'
+
